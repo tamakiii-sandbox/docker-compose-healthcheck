17e8d558e438f64ba24ba937b73d0d74e9b73d8e
tamakiii <tamakiii@users.noreply.github.com>
2019-06-24 19:12:54 +0900

    fix

--- test/run.sh
+++ test/run.sh
@@ -3,6 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 CONTAINER=$1
+CMD=$2
 
 COUNT=0
 RETRIES=5
@@ -14,7 +15,7 @@ while [ $COUNT -lt $RETRIES ]; do
   echo "[info] status: $STATUS";
 
   if [ "$STATUS" = "healthy" ]; then
-    make -C $DIR
+    exec $CMD;
     break;
   fi
 
